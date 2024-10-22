36f848f0cd22a01551381af470970043474e814b
stiephl6204 <stiephl6204_aehit@students.vizja.pl>
2024-10-23 00:12:21 +0200

    Faster

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -1,13 +1,21 @@
 #include <vector>
 #include "Divisors.h"
+#include <algorithm> // для сортировки
 
-std::vector<unsigned long long> divisors(unsigned long long number)
-{
-	std::vector<unsigned long long> result;
-	for (unsigned long long i = 1; i <= number; ++i)
-	{
-		if (number % i == 0)
-			result.push_back(i);
-	}
-	return result;
+std::vector<unsigned long long> divisors(unsigned long long number) {
+    std::vector<unsigned long long> result;
+    
+    for (unsigned long long i = 1; i * i <= number; ++i) {
+        if (number % i == 0) {
+            result.push_back(i);               // Добавляем делитель i
+            if (i != number / i) {
+                result.push_back(number / i);  // Добавляем парный делитель N / i
+            }
+        }
+    }
+
+    // Сортируем делители по возрастанию
+    std::sort(result.begin(), result.end());
+
+    return result;
 }
